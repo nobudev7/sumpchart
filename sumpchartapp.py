aba6326613ec8911c36f042b1b46b9df6352266a
Nobu Tamagawa <97799865+ntamagawa@users.noreply.github.com>
2023-11-04 12:44:47 -0400

    Show latest day data on chart. Make X axis look better. Make water level in cm.

--- sumpchartapp.py
+++ sumpchartapp.py
@@ -1,14 +1,48 @@
 import streamlit as st
 import requests
 import pandas as pd
+import datetime
+import altair as alt
 
-st.title('Sump Pump Data')
 
-url = 'http://192.168.1.169:8080/rest/all'
+# Obtain the latest date of the data
+url = 'http://localhost:8080/devices/1/entries/'
 response = requests.get(url)
-if response.status_code == 200:
-    json_data = response.json()
-    df = pd.DataFrame(json_data)
-    st.line_chart(df, x='measuredOn', y='value')
-else:
+if response.status_code != 200:
     st.write('Response code: ' + str(response.status_code))
+    raise SystemExit
+
+entry = response.json()
+dt = datetime.datetime.fromisoformat(entry.get("measuredOn"))
+
+st.header("Sump Pump Data")
+
+response = requests.get(url + dt.strftime('%Y/%m/%d'))
+if response.status_code != 200:
+    st.write('Response code: ' + str(response.status_code))
+    raise SystemExit
+
+entries_json = response.json()
+entries_df = pd.DataFrame(entries_json)
+#st.line_chart(entries_df, x='measuredOn', y='value')
+
+entries_df['time'] = pd.to_datetime(entries_df['measuredOn']).dt.strftime("%H:%M:%S")
+entries_df['waterlevel'] = entries_df['value'].div(10)
+#st.write(entries_df)
+
+chart = (
+        alt.Chart(
+            data=entries_df,
+            title="Water Level on " + dt.strftime('%Y/%m/%d'),
+        )
+        .mark_line()
+        .encode(
+            x=alt.X("time", axis=alt.Axis(title="Time")),
+            y=alt.Y("waterlevel", axis=alt.Axis(title="Water Level (cm)")),
+        )
+)
+
+st.altair_chart(chart, use_container_width=True)
+
+
+
