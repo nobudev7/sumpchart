732350294d26b66f3c14e2cb427815ee2643133d
Nobu Tamagawa <97799865+ntamagawa@users.noreply.github.com>
2023-11-05 13:31:23 -0500

    Add date picker and update chart when a new date is selected
    TODO: Error handling when the water level data is not available for selected date

--- sumpchartapp.py
+++ sumpchartapp.py
@@ -5,35 +5,47 @@ import datetime
 import altair as alt
 
 
+base_url = 'http://localhost:8080/devices/1/entries'
+
 # Obtain the latest date of the data
-url = 'http://localhost:8080/devices/1/entries'
-response = requests.get(url)
+response = requests.get(base_url)
 if response.status_code != 200:
+    st.write('URL: ' + base_url)
     st.write('Response code: ' + str(response.status_code))
     raise SystemExit
 
 entry = response.json()
 dt = datetime.datetime.fromisoformat(entry.get("measuredOn"))
+request_path = dt.strftime('%Y/%m/%d')
+
 
 st.header("Sump Pump Data")
+entries_df = pd.DataFrame()
 
-response = requests.get(url + "/" + dt.strftime('%Y/%m/%d'))
-if response.status_code != 200:
-    st.write('Response code: ' + str(response.status_code))
-    raise SystemExit
+def update_data():
+    global entries_df
+    response = requests.get(base_url + "/" + selected_date.strftime("%Y/%m/%d"))
+    if response.status_code != 200:
+        st.write('URL: ' + base_url + "/" + selected_date.strftime("%Y/%m/%d"))
+        st.write('Response code: ' + str(response.status_code))
+        raise SystemExit
 
-entries_json = response.json()
-entries_df = pd.DataFrame(entries_json)
-#st.line_chart(entries_df, x='measuredOn', y='value')
+    entries_json = response.json()
+    entries_df = pd.DataFrame(entries_json)
+    # st.line_chart(entries_df, x='measuredOn', y='value')
 
-entries_df['time'] = pd.to_datetime(entries_df['measuredOn']).dt.strftime("%H:%M:%S")
-entries_df['waterlevel'] = entries_df['value'].div(10)
-#st.write(entries_df)
+    entries_df['time'] = pd.to_datetime(entries_df['measuredOn']).dt.strftime("%H:%M:%S")
+    entries_df['waterlevel'] = entries_df['value'].div(10)
+    #st.write(entries_df)
+
+selected_date = st.sidebar.date_input(label = "Select", value = dt, on_change=update_data)
+
+update_data()
 
 chart = (
         alt.Chart(
             data=entries_df,
-            title="Water Level on " + dt.strftime('%Y/%m/%d'),
+            title="Water Level on " + selected_date.strftime("%Y/%m/%d"),
         )
         .mark_line()
         .encode(
